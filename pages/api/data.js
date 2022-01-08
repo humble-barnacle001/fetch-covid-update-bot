6767220decd95cb86b836ff5754a08ecd18a9157
Rwitick Ghosh <58736240+humble-barnacle001@users.noreply.github.com>
2022-01-09 02:55:28 +0530

    Correct Typo in API handler

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,7 +1,7 @@
 import fetchUpdate from "../../components/updateService/fetchUpdate"
 import { parseAsEndpoint } from "../../components/utils/parser"
 
-export async function handler(req, res) {
+export default async function handler(req, res) {
     const response = await fetchUpdate();
     return res.json(parseAsEndpoint(response));
 }
