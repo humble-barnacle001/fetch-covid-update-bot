29803ec274eaca18b7cf4c229d24c17dbd76d411
Barnacle Nightshade <58736240+humble-barnacle001@users.noreply.github.com>
2022-01-08 15:36:24 +0000

    Add SEO specific properties

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../assests/github.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <title>Query COVID-19 India Cases Bot</title>
+                <meta
+                    property='description'
+                    content='Query COVID-19 daily cases count in Telegram'
+                />
+            </Head>
             <Component {...pageProps} />
         </>
     );
