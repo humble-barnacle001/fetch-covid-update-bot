95128d80f348e93f6925fa80781c11aa1f06f1ee
Rwitick Ghosh <58736240+humble-barnacle001@users.noreply.github.com>
2022-01-09 03:05:26 +0530

    Allow CORS

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,6 +2,17 @@ import fetchUpdate from "../../components/updateService/fetchUpdate"
 import { parseAsEndpoint } from "../../components/utils/parser"
 
 export default async function handler(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    if (req.method == "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).end();
+    }
+
     const response = await fetchUpdate();
     return res.json(parseAsEndpoint(response));
 }
